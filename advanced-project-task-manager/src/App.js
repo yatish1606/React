6d5351e05a84635c4bf4ca850e0e01b0ba0c1177
Yatish Kelkar <yatish1606@gmail.com>
2020-06-23 20:27:51 +0530

    Add SideBar component to advanced-project-task-manager

--- advanced-project-task-manager/src/App.js
+++ advanced-project-task-manager/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Header} from './layout/Header.js'
+import {Content} from './layout/Content'
 
 export const App = () => (
     <div className="App">
       <Header/>
+      <Content/>
     </div>
 );
 
---
+++ advanced-project-task-manager/src/layout/SideBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {FaChevronDown, FaInbox, FaRegCalendar, FaRegCalendarAlt} from 'react-icons/fa'
+
+export const SideBar = () => {
+    return (
+        <div className='sidebar' data-testid='sidebar'>
+            <ul className='sidebar__generic'>
+                <li><span><FaInbox/></span><span>Inbox</span></li>
+                <li><span><FaRegCalendar/></span><span>Today</span></li>
+                <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
+            </ul>
+
+            <div className='sidebar__middle'>
+                <span><FaChevronDown/></span>
+                <h2>Projects</h2>
+            </div>
+
+            <ul className='sidebar__projects'>Projects are here</ul>
+            Add project here
+        </div>
+    )
+}
