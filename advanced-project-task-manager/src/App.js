07c48d8d395ba3cb9626f42c982098a31ab6caf0
Yatish Kelkar <yatish1606@gmail.com>
2020-06-24 21:28:43 +0530

    Added task and checkbox layouts

--- advanced-project-task-manager/src/App.js
+++ advanced-project-task-manager/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Header} from './layout/Header.js'
-import {Content} from './layout/Content'
+import {Header} from './components/layout/Header.js'
+import {Content} from './components/layout/Content'
 
 export const App = () => (
     <div className="App">
