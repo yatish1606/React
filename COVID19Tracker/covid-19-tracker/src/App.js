3a51d9ad6daeb1695ad3082f828ca6af73726b98
Yatish Kelkar <yatish1606@gmail.com>
2020-06-30 13:00:27 +0530

    Added componenets

--- COVID19Tracker/covid-19-tracker/src/App.js
+++ COVID19Tracker/covid-19-tracker/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>
-          App
-        </h1>
+      <div className={styles.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
       </div>
     )
   }
---
+++ COVID19Tracker/covid-19-tracker/src/components/countryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryPicker = () => {
+    return (
+        <h1>
+            COuntry pIcker
+        </h1>
+    )
+} 
+
+export default CountryPicker
