604a4380fb2d72203d07f1a20196ce546e39752c
Yatish Kelkar <yatish1606@gmail.com>
2020-07-16 13:27:32 +0530

    Added func for download file using ObjectID

--- FileShare/api/src/router.js
+++ FileShare/api/src/router.js
@@ -3,6 +3,7 @@ import path from 'path'
 import {version} from '../package.json'
 import _ from 'lodash'
 import File from './models/file'
+import {ObjectID} from 'mongodb'
 
 class AppRouter {
     constructor(app) {
@@ -41,7 +42,6 @@ class AppRouter {
                     if(err){
                         return res.status(503).json({ message: 'Unable to upload file'})
                     }
-                    console.log(chalk.green('Saved files with results ', err, result))
                     return res.json({
                         file: fileModels
                     })
@@ -49,27 +49,36 @@ class AppRouter {
             } else {
                 return res.status(503).json({ error: 'File upload is required'})
             }
-
-            return res.json({
-                files : fileModels
-            })
         })
 
         // Download file
-        app.get('/api/download/:name', (req, res, next) => {
-            const fileName = req.params.name
-            const filePath = path.join(uploadDirectory,fileName)
-            return res.download(filePath, fileName, err => {
-                if(err){
-                    return res.json({
-                        error:{
-                            message:'File not found'
-                        }
-                    })
-                } else {
-                    console.log(chalk.green('File is successfully downloaded'))
+        app.get('/api/download/:id', (req, res, next) => {
+
+            const fileID = req.params.id
+
+            db.collection('files').find({ _id: ObjectID(fileID) }).toArray((err, result) => {
+
+                console.log(chalk.white('Searching for file', err, result))
+
+                if(err || !result){
+                    return res.status(404).json({ error : {message:'File not found'}})
                 }
-            })
+
+                const fileName = _.get(result, '[0].name')
+                const filePath = path.join(uploadDirectory,fileName)
+
+                return res.download(filePath, fileName, err => {
+                    if(err){
+                        return res.json({
+                            error:{
+                                message:'File not found'
+                            }
+                        })
+                    } else {
+                        console.log(chalk.green('File is successfully downloaded'))
+                    }
+                })
+            }) 
         })
         
         console.log(chalk.green('App Routing is set up'))
