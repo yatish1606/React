dfe4ef3eb3bf91dc83507a747ea7122bd9f95127
Yatish Kelkar <yatish1606@gmail.com>
2020-07-17 13:00:56 +0530

    Added sass styles for layout

--- FileShare/app/src/index.js
+++ FileShare/app/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Layout from './layout'
 import * as serviceWorker from './serviceWorker';
+import './css/app.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ FileShare/app/src/layout/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+export default class Layout extends Component {
+    constructor (props) {
+        super(props)
+    }
+
+    render () {
+        return (
+            <div className="app-layout">
+                <div className="app-container">
+                    <div className="app-left">
+
+                    </div>
+                    <div className="app-content">
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
