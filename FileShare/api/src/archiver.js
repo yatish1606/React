d30ed52272597dc5b3594ca26353ed968a4fb171
Yatish Kelkar <yatish1606@gmail.com>
2020-08-03 15:20:05 +0530

    Zip file download from S3 complete

--- FileShare/api/src/archiver.js
+++ FileShare/api/src/archiver.js
@@ -1,6 +1,7 @@
 import archiver from 'archiver'
 import _ from 'lodash'
 import path from 'path'
+import S3 from './s3'
 
 export default class FileArchiver {
     constructor(app, files = [], response) {
@@ -11,16 +12,22 @@ export default class FileArchiver {
 
     downloadFiles() {
         const files = this.files
-        const uploadDirectory = this.app.get('storageDirectory')
+        //const uploadDirectory = this.app.get('storageDirectory')
         const zip = archiver('zip')
         const response = this.response
+        const app = this.app
 
         response.attachment('download.zip')
         zip.pipe(response)
+
+        const s3Downloader = new S3(app, response)
  
         _.each(files, file => {
-            const filePath = path.join(uploadDirectory, _.get(file, 'name'))
-            zip.file(filePath, {name :  _.get(file, 'originalName')})
+            // const filePath = path.join(uploadDirectory, _.get(file, 'name'))
+            // zip.file(filePath, {name :  _.get(file, 'originalName')})
+
+            const fileObject = s3Downloader.getObject(file)
+            zip.append(fileObject, {name :  _.get(file, 'originalName')})
         })
 
         zip.finalize()
