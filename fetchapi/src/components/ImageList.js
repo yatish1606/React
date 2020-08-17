bc1ee06a071fb5ae908a6e54eca2287c6b4f758b
Yatish Kelkar <yatish1606@gmail.com>
2020-08-17 15:27:12 +0530

    Add imageList css for fetchapi

--- fetchapi/src/components/ImageList.js
+++ fetchapi/src/components/ImageList.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import './imageList.css'
 
 class ImageList extends React.Component {
 
 
     constructor(props) {
-        super(props)
-
-        this.componentDidMount = async () => {
-            const images = await this.props.images
-            console.log(images)
-        }
+        super(props)     
     }
 
     render() {
+            
+        const images = this.props.images.map(({id,urls,description}) => {
+            return (
+                <div key={id}>
+                    <img src={urls.regular} alt={description}/>
+                </div>
+                
+            )
+        })
+
         return (
-            <div>
-                this is image list
-                found {this.props.images.length} images
+            <div className='image-list'>
+                {images}
             </div>
         )
     }
