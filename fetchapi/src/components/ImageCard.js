6a6ce371c4a055651f38689f565be7af283d8886
Yatish Kelkar <yatish1606@gmail.com>
2020-08-17 18:02:12 +0530

    fetchapi complete

--- fetchapi/src/components/ImageCard.js
+++ fetchapi/src/components/ImageCard.js
@@ -4,22 +4,32 @@ import './imageList.css'
 class ImageCard extends React.Component {
 
     constructor(props){
+        
         super(props)
 
         this.state = {
-            image : null
+            spans : 0
         }
-        
         this.imageRef = React.createRef()
-
-        this.UNSAFE_componentWillReceiveProps = (nextProps) => {
-            this.setState({image : nextProps})
-        }
+        this.componentDidMount = this.componentDidMount.bind(this)      
     }
+
+    componentDidMount = function () {
+        this.imageRef.current.addEventListener('load', this.setSpan)
+    }
+
+    setSpan = () => {
+        const clientHeight = this.imageRef.current.clientHeight
+        const spans = Math.ceil(clientHeight/10 + 1)
+        this.setState({spans})
+    }
+
     render() {
-        const {image} = this.state
+        const {image} = this.props
         return (
-            <div>
+            <div
+                style={{gridRowEnd: `span ${this.state.spans}`}}
+            >
                 <img
                     ref={this.imageRef}
                     src={image.urls.regular} 
