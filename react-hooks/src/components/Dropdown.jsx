db41053eb07ddf43e04b3796296ffc15d83e5257
Yatish Kelkar <yatish1606@gmail.com>
2020-08-26 22:29:11 +0530

    Fixed dropdown issue

--- react-hooks/src/components/Dropdown.jsx
+++ react-hooks/src/components/Dropdown.jsx
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const Dropdown = ({options = [], selectedColor, setSelectedColor}) => {
 
     const [open, setOpen] = useState(false)
+    const ref = useRef()
 
     useEffect(() => {
-        document.body.addEventListener('click', () => {
+        document.body.addEventListener('click', (event) => {
+            if(ref.current.contains(event.target)) 
+                return
             setOpen(false)
         })
-    }, [])
+    }, []) 
+
+    //    Note for event listeners : event listeners in a components do not necessarily get called 
+    //    from child to parent(bubbling up). First, all listeners set up manually using 
+    //    node.addEventListener() get called, followed by listeners set up inside React JSX, like 
+    //    onClick={() => doSomething()}. If there are multiple listeners inside React, then they will be called 
+    //    from chile to parent upwards. 
+        
 
     const renderedItems = options
         .filter(option => option !== selectedColor)
@@ -24,8 +34,9 @@ const Dropdown = ({options = [], selectedColor, setSelectedColor}) => {
             )
         })
 
+
     return (
-        <div className="ui form">
+        <div className="ui form" ref={ref}>
             <div className="field">
                 <label className="label">Select color</label>
                 <div 
