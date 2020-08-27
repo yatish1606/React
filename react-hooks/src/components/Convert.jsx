534b98a9f8644c315ab7d8317441273f2e1c9fbf
Yatish Kelkar <yatish1606@gmail.com>
2020-08-27 10:16:34 +0530

    Add navigation in react-hooks

--- react-hooks/src/components/Convert.jsx
+++ react-hooks/src/components/Convert.jsx
@@ -5,12 +5,17 @@ import {GOOGLE_TRANSLATE_API_KEY} from '../config'
 const Convert = ({language, text}) => {
 
     const [translatedLanguage, setTranslatedLanguage] = useState('')
+    const [debouncedText, setDebouncedText] = useState('')
     const url = 'https://translation.googleapis.com/language/translate/v2'
 
+    const timeoutID = setTimeout(() => {
+        setDebouncedText(text)
+    }, 500)
+
     useEffect(() => {
-        
+
         (async () => {
-            const response = await axios.get(url, {
+            const response = await axios.post(url, {}, {
                 params: {
                     q: text,
                     target: language.value,
@@ -20,7 +25,11 @@ const Convert = ({language, text}) => {
             setTranslatedLanguage(response.data.data.translations[0].translatedText)
         })()
 
-    }, [language, text])
+        return () => {
+            clearTimeout(timeoutID)
+        }
+
+    }, [language, debouncedText])
 
     return (
         <div>{translatedLanguage}</div>
