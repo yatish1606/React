3827972cf8c8b422c4933f08f0f8103904ac2b7f
Yatish Kelkar <yatish1606@gmail.com>
2020-08-30 12:32:11 +0530

    Add fetch request in blog-list

---
+++ blog-list/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostList from './PostList'
+
+const App = () => {
+    return(
+        <div>
+            this is app
+            <PostList/>
+        </div>
+    )
+}
+
+export default App
---
+++ blog-list/src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>
+                this is postlist
+            </div>
+        )
+    }
+}
+
+export default PostList
