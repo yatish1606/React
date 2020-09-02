4bb6fe58e8cf91cac061b2f963ea662e5b156c9a
Yatish Kelkar <yatish1606@gmail.com>
2020-09-02 11:03:41 +0530

    Refactor _.memoize() with useMemo() hook

--- blog-list/src/actions/index.js
+++ blog-list/src/actions/index.js
@@ -1,5 +1,5 @@
 import JSONPlaceholder from '../api/JSONPlaceholder'
-
+import _ from 'lodash'
 // Async await doesnt work for action creators
 
 export const fetchPosts = () => async dispatch => {
@@ -7,7 +7,11 @@ export const fetchPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload : response.data})
 }
 
-export const fetchUser = id => async dispatch => {
-    const response = await JSONPlaceholder.get(`/users/${id}`)
-    dispatch({type: 'FETCH_USER', payload : response.data})
-}
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+
+
+const _fetchUser = _.memoize(async function (id, dispatch) {
+        const response = await JSONPlaceholder.get(`/users/${id}`)
+        dispatch({type: 'FETCH_USER', payload : response.data})
+    },undefined)
+
