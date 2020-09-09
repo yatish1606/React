18a49880f7665b2959e122fffbe0c6b1fafa06b2
Yatish Kelkar <yatish1606@gmail.com>
2020-09-09 18:38:13 +0530

    Complete GoogleOAuth

--- twitch-clone/app/src/components/commons/GoogleAuthentication.jsx
+++ twitch-clone/app/src/components/commons/GoogleAuthentication.jsx
@@ -3,20 +3,47 @@ import { GOOGLE_OAUTH_CLIENT_ID } from '../../config'
 
 class GoogleAuthentication extends React.Component {
 
+    state = {
+        isSignedIn : null
+    }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId : GOOGLE_OAUTH_CLIENT_ID,
                 scope : 'email'
             })
+            .then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({ isSignedIn : this.auth.isSignedIn.get()})
+                this.auth.isSignedIn.listen(this.onAuthStateChange)
+            })
         })
     }
 
+    onAuthStateChange = () => this.setState({ isSignedIn : this.auth.isSignedIn.get()})
+
+    renderAuthButton() {
+        switch(this.state.isSignedIn) {
+            case null :
+                return (
+                    <div>null</div>
+                )
+            case true :
+                return (
+                    <div>true</div>
+                )
+            default :
+                return (
+                    <div>false</div>
+                )
+        }
+    }
 
     render() {
         return (
             <div>
-                Login
+                {this.renderAuthButton()}
             </div>
         )
     }
