c2342f7ff2384e59a093fcb5cc41b2a4272bf1cd
Yatish Kelkar <yatish1606@gmail.com>
2020-09-10 15:47:45 +0530

    Google Sign in sign out complete

--- twitch-clone/app/src/components/commons/GoogleAuthentication.jsx
+++ twitch-clone/app/src/components/commons/GoogleAuthentication.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { LogIn } from 'react-feather';
+import { LogIn, LogOut } from 'react-feather';
+import OutsideClickHandler from 'react-outside-click-handler'
 
 import { GOOGLE_OAUTH_CLIENT_ID } from '../../config'
 
 import '../../css/app.css'
+import '../../css/Header.css'
 
 class GoogleAuthentication extends React.Component {
 
     state = {
         isSignedIn : null,
-        profileInfo : null
+        profileInfo : null,
+        showAccount : false
+    }  
+    
+    constructor(props) {
+        super(props);
+
+        this.wrapperRef = React.createRef();
+        // this.setWrapperRef = this.setWrapperRef.bind(this);
+        // this.handleClickOutside = this.handleClickOutside.bind(this);
     }
 
+        
     componentDidMount() {
+        
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId : GOOGLE_OAUTH_CLIENT_ID,
@@ -33,7 +46,7 @@ class GoogleAuthentication extends React.Component {
                         profilePhoto : profileInfo.jK
                     }
                     this.setState({profileInfo : profile})
-                    console.log(this.state.profileInfo)
+                    
                 }
             })
         })
@@ -42,9 +55,13 @@ class GoogleAuthentication extends React.Component {
 
     onAuthStateChange = () => this.setState({ isSignedIn : this.auth.isSignedIn.get()})
 
-    googleSignOut = () => window.gapi.auth2.getAuthInstance().signOut()
+    googleSignOut = () => {
+        window.gapi.auth2.getAuthInstance().signOut()
+    }
 
-    googleSignIn = () => window.gapi.auth2.getAuthInstance().signIn()
+    googleSignIn = () => {
+        window.gapi.auth2.getAuthInstance().signIn()
+    }        
 
     renderAuthButton() {
         switch(this.state.isSignedIn) {
@@ -53,21 +70,22 @@ class GoogleAuthentication extends React.Component {
                     <div>null</div>
                 )
             case true :
-                return (
-                    <div>
-                        {/* <button 
-                            className="btn primary"
-                            onClick={() => this.googleSignOut()}
-                        >Sign In</button> */}
-                        true
+                return this.state.profileInfo ? (
+                    <div className="header-profile" onClick={() => this.setState({showAccount : !this.state.showAccount})}>
+                        <div className="header-profile-photo">
+                            <img src={this.state.profileInfo.profilePhoto}/>
+                        </div>
+                        <div className="header-name">
+                            {this.state.profileInfo.firstName}
+                        </div>
                     </div>
-                )
+                ) : null
             default :
                 return (
                     <div>
                         <button 
                             className="btn primary"
-                            onClick={() => this.googleSignIn()}
+                            onClick={this.googleSignIn}
                         >
                             <LogIn className="btn-icon" size={18} />
                             Sign In
@@ -79,11 +97,25 @@ class GoogleAuthentication extends React.Component {
 
     render() {
     
-        
+        console.log(this.state.showAccount)
 
         return (
             <div>
+
                 {this.renderAuthButton()}
+
+                {
+                    this.state.showAccount ? 
+                    <OutsideClickHandler onOutsideClick={() => this.setState({ showAccount : false})}>
+                    <div className='my-profile-dropdown-container'  ref={node => this.wrapperRef = node}>
+                        <button className="btn primary" onClick={this.googleSignOut}>
+                            <LogOut className="btn-icon" size={18} />
+                            Sign Out
+                        </button>
+                    </div>
+                    </OutsideClickHandler>
+                    : null
+                }
             </div>
         )
     }
--- twitch-clone/app/src/components/commons/Header.jsx
+++ twitch-clone/app/src/components/commons/Header.jsx
@@ -8,25 +8,29 @@ import GoogleAuthentication from './GoogleAuthentication'
 
 const Header = () => {
     return (
-        <div className="header-container">
-            <Link to='/'>
-                <div className="header-logo">
-                    logo
-                </div>
-            </Link>
-            <span className="header-other">
+        <div>
+            <div className="header-container">
                 <Link to='/'>
-                    <div className="header-settings header-other-item">
-                        <Settings className="btn-round" size={20} color='grey'/>
+                    <div className="header-logo">
+                        logo
                     </div>
-                </Link>  
-                <Link to='/'>
-                    <div className="header-login header-other-item">
-                        <GoogleAuthentication/>
-                    </div>
-                </Link>  
-            </span>
+                </Link>
+                <span className="header-other">
+                    <Link to='/'>
+                        <div className="header-settings header-other-item">
+                            <Settings className="btn-round" size={20} color='grey'/>
+                        </div>
+                    </Link>  
+                    <Link to='/' >
+                        <div className="header-login header-other-item">
+                            <GoogleAuthentication/>
+                        </div>
+                    </Link>  
+                </span>
+            </div>
+            
         </div>
+        
     )
 }
 
