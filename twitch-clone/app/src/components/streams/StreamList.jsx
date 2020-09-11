ebdc76dcbe5127cfa27e8267ca1ce48bc1d1b6fd
Yatish Kelkar <yatish1606@gmail.com>
2020-09-11 20:10:39 +0530

    Render all streams

--- twitch-clone/app/src/components/streams/StreamList.jsx
+++ twitch-clone/app/src/components/streams/StreamList.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    )
+import { fetchStreams } from '../../actions'
+
+class StreamList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStreams()
+    }
+
+    render() {
+        return (
+            <div>
+                StreamList
+            </div>
+        )
+    }
 }
 
-export default StreamList
+export default connect(null, {fetchStreams})(StreamList)
