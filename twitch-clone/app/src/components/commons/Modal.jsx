b156dd1348f1dd93ac971f9d27089562da36c9d0
Yatish Kelkar <yatish1606@gmail.com>
2020-09-28 15:43:42 +0530

    Generalised modal

--- twitch-clone/app/src/components/commons/Modal.jsx
+++ twitch-clone/app/src/components/commons/Modal.jsx
@@ -5,7 +5,20 @@ import history from '../../history'
 import '../../css/modal.css'
 
 class Modal extends React.Component {
+
+    renderActionButtons = actions => {
+        return actions.map(action => {
+            return (
+                <button className={`btn ${action.class} no-icon`}>{action.name}</button>
+            )
+        })
+    }
+
     render() {
+        console.log(this.props)
+
+        const { title, description, actions } = this.props
+
         return ReactDOM.createPortal(
             <div 
                 className="modal-background" 
@@ -16,7 +29,12 @@ class Modal extends React.Component {
                     className="modal-content-container"
                     onClick={event => event.stopPropagation()}
                 >
-    
+                    {/* Content goes below */}
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <div className="modal-actions">
+                       {this.renderActionButtons(actions)}
+                    </div>
                 </div>
             </div>,
             document.querySelector('#modal-root')
--- twitch-clone/app/src/components/streams/StreamDelete.jsx
+++ twitch-clone/app/src/components/streams/StreamDelete.jsx
@@ -4,7 +4,14 @@ import Modal from '../commons/Modal'
 const StreamDelete = () => {
     return (
         <div>
-            <Modal/>
+            <Modal
+                title="Delete Stream?"
+                description="Are you sure you want to delete this stream?"
+                actions = {[
+                    {name: 'Cancel', class: 'secondary', action:'goback'},
+                    {name:'Delete', class:'primary'},
+                ]}
+            />
         </div>
     )
 }
